e52146fdf44f9639d9fb3286629d25fd11f738de
Ulrich Berthold <ub@artfacts.net>
2021-05-25 13:24:53 +0200

    pass JEKYLL_ENV from workflow parameter to build, default to 'production'

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 set -e
 
+if [ -n "${INPUT_JEKYLL_ENV}" ]; then
+      echo "::debug::Environment is set via input parameter"
+  else
+      echo "::debug::Environment default in use - production"
+      INPUT_JEKYLL_ENV="production"
+fi
+
 echo "Installing gems"
 bundle config path vendor/bundle
 bundle install
 echo "Gems installed"
 
 echo "Jekyll building"
-bundle exec jekyll build
+JEKYLL_ENV=${INPUT_JEKYLL_ENV} bundle exec jekyll build
 echo "Jekyll build done"
 
